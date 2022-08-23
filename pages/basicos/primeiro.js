f55283cc6cc9d45d5cd43f1e4d6e4f73fae353bd
JordanoBaluz <jordanolb95@hotmail.com>
2022-08-23 20:54:11 -0300

    criado primeiros componentes para treinar funcionamento dos componentes

---
+++ pages/basicos/primeiro.js
@@ -0,0 +1,6 @@
+export default function primeiro() {
+  return "Primeiro componente";
+}
+
+// ao criar dentro de pages, é criado um caminho de url de acesso
+// a estrutura de pastas e arquivos dentro de pages influencia o caminho da url
---
+++ pages/basicos/segundo.js
@@ -0,0 +1,5 @@
+export default function segundo() {
+  return <h1>Segundo Componente</h1>;
+}
+
+// componente em JSX onde o NextJS trabalha por padrão
