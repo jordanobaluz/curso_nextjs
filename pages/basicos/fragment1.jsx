4c3c1d2d5e811bbfc3ca14484c0f0fd53fe741ee
JordanoBaluz <jordanolb95@hotmail.com>
2022-08-23 21:26:42 -0300

    componente de demonstração do fragment em componentes

---
+++ pages/basicos/fragment1.jsx
@@ -0,0 +1,10 @@
+export default function fragmento1() {
+  return (
+    <>
+      <h1>Fragmento</h1>
+      <h2>subtitulo fragmento</h2>
+    </>
+  );
+}
+
+// fragmento é utilizado quando precisa retornar mais de 1 elemento sem utilizar div
