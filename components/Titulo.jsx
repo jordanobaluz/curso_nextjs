609af0b5b9ffaacd8dc08bc36be9d1b778e23882
JordanoBaluz <jordanolb95@hotmail.com>
2022-08-23 21:38:01 -0300

    trabalhando com export e import de componentes

---
+++ components/Titulo.jsx
@@ -0,0 +1,10 @@
+export default function Titulo() {
+  return (
+    <>
+      <h1>Titulo fora da pages</h1>
+      <h2>SubTitulo</h2>
+    </>
+  );
+}
+
+// nome com letra maiscula em arquivos fora da pasta pages
---
+++ pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/titulo";
+
+export default function usandoTitulo() {
+  return (
+    <div>
+      <h1>Usando Titulo</h1>
+      <Titulo />
+    </div>
+  );
+}
