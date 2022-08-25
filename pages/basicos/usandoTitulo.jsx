cbf696e95c7a18945ed0c1d297cc1d2c9e604d46
JordanoBaluz <jordanolb95@hotmail.com>
2022-08-24 21:56:02 -0300

    passagem de props e como utilizar renderização com props

--- pages/basicos/usandoTitulo.jsx
+++ pages/basicos/usandoTitulo.jsx
@@ -4,7 +4,15 @@ export default function usandoTitulo() {
   return (
     <div>
       <h1>Usando Titulo</h1>
-      <Titulo />
+      <Titulo
+        principal="Página de Cadastro"
+        secundario="Incluir, alterar e excluir coisas!"
+        pequeno={true}
+      />
+      <Titulo
+        principal="Página de login"
+        secundario="informe seu login e senha"
+      />
     </div>
   );
 }
