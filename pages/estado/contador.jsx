ff967ed9583179ecaa873a8b318ad90945653635
JordanoBaluz <jordanolb95@hotmail.com>
2022-08-29 22:07:26 -0300

    criação de um componente contador para realizar o desafio do curso

---
+++ pages/estado/contador.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function contador() {
+    const estilo = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#222",
+        color: "#FFF",
+        height: "100vh"
+    }
+
+    const [valor, setValor] = useState(0);
+
+    return (
+        <div style={estilo}>
+            <h1>Contador</h1>
+            <div style={{
+                display: "flex",
+                width: "250px",
+                justifyContent: "space-around",
+                fontSize: "2rem"
+            }}>
+                <button onClick={() => setValor(valor - 1)}>-</button>
+                <span>Valor: {valor}</span>
+                <button onClick={() => setValor(valor + 1)}>+</button>
+            </div>
+        </div>
+    )
+}
